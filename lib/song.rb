13afa8eaeb730fedff0d054f24c720e2d5b5f4d5
Marc Mills <mdmills1990@gmail.com>
2018-09-05 13:10:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,7 @@ class Song
 
 
   def self.find_or_create_by_name(name)
-    if find_by_name(name).name == name
+    if find_by_name(name) == name
       find_by_name(name)
     else
       create_by_name(name)
